f5bea85105af3e4b9ac9213c8425def81b58446f
Anknguyen <97307551+Anknguyen@users.noreply.github.com>
2024-11-20 22:27:16 -0500

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders About component on default route', () => {
-  render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>
-  );
+  render(<App />);
 
   // Check for the presence of the About component content
   const aboutText = screen.getByText(/Hi! I'm An Nguyen/i); // Adjust this text to match actual content in About component
