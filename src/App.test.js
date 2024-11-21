d80b2b4e64e24d33c70365c4aaff84cd9f8778b3
Anknguyen <97307551+Anknguyen@users.noreply.github.com>
2024-11-20 22:30:43 -0500

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,15 @@
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
-test('renders About component on default route', () => {
-  render(<App />);
+test('renders About component on default route', async () => {
+  await act(async () => {
+    render(<App />);
+  });
 
   // Check for the presence of the About component content
-  const aboutText = screen.getByText(/Hi! I'm An Nguyen/i); // Adjust this text to match actual content in About component
+  const aboutText = screen.getByText(/Passionate software developer/i); // Adjust this text to match actual content in About component
   expect(aboutText).toBeInTheDocument();
 });
